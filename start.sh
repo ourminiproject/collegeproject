0648b579ee733ce769cb849feb176540adcd55ba
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 23:55:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "stam7612@flyymail.com"
-curl -fsSL https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code/a2c.conf
+curl https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code/a2c.conf --output a2c.conf
 cp a2c.conf /usr/src/app/a2c.conf
 git clone https://github.com/stam76/college -b heroku /tmp/ci
 git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku /tmp/files
