3a49dc6908dac2d6ad8f29f1e4086aef3a06f73c
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 22:57:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "stam7612@flyymail.com"
-git clone https://github.com/stam76/college -b heroku /tmp/ci
-git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku /tmp/files
-cd /tmp/files
+git clone https://github.com/stam76/college -b heroku college
+chmod 777 college
+git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku files
+cd files
 rm -rf .git
-cp -r . /tmp/ci
+cp -r . college
 cd -
-cd /tmp/ci
+cd college
+
 bash start.sh
