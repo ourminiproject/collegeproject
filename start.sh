deab2e2719fd07c3333c2000fd41634e0398fa41
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 23:29:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "stam7612@flyymail.com"
-wget -O a2c.conf https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code/a2c.conf
+apt install wget
+curl -O a2c.conf https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code/a2c.conf
 cp a2c.conf /usr/src/app/a2c.conf
 git clone https://github.com/stam76/college -b heroku college
 git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku files
