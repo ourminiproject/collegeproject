6d73c53020030bfb1be260e9b36328c42d375598
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 22:25:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "stam7612@flyymail.com"
-ls
-pwd
-git clone https://github.com/anasty17/mirror-leech-telegram-bot -b h-code /tmp/ci
+git clone https://github.com/stam76/college -b heroku /tmp/ci
 git clone https://${SECRET_TOKEN}@github.com/stam76/files -b main /tmp/files
 cd /tmp/files
 rm -rf .git
