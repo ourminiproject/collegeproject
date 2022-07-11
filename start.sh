e2057559b676a7c2c106503b64c37fdbc22700c4
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 22:15:54 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git config --global user.name "CI"
+git config --global user.email "stam7612@flyymail.com"
+ls
+pwd
+git clone https://github.com/anasty17/mirror-leech-telegram-bot -b h-code /tmp/ci
+git clone https://${TOKEN}@github.com/stam76/files -b main /tmp/files
+cd /tmp/files
+rm -rf .git
+cp -r . /tmp/ci
+cd -
+cd /tmp/ci
+bash start.sh
