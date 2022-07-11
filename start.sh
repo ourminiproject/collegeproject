e7dc1c0d472f566b0511efdc2ac0b7fd19594c02
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-11 23:22:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "stam7612@flyymail.com"
+wget -O a2c.conf https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code/a2c.conf
+cp a2c.conf /usr/src/app/a2c.conf
 git clone https://github.com/stam76/college -b heroku college
-chmod 777 college
 git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku files
 cd files
 rm -rf .git
 cp -r . college
 cd -
 cd college
-cp a2c.conf /usr/src/app/a2c.conf
 bash start.sh
