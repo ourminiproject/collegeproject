65020ad7fba1ecfb9fa50b6811da73db7e1d4b17
stam76 <108683281+stam76@users.noreply.github.com>
2022-07-12 02:24:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,12 @@ curl https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/h-code
 cp a2c.conf /usr/src/app/a2c.conf
 rm -rf /tmp/ci
 rm -rf /tmp/files
-git clone https://github.com/stam76/college -b heroku /tmp/ci
+git clone https://github.com/arshsisodiya/helios-mirror -b heroku /tmp/ci
 git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku /tmp/files
 cd /tmp/files
 rm -rf .git
 cp -r . /tmp/ci
 cd -
+chmod 777 /tmp/ci
 cd /tmp/ci
 bash start.sh
