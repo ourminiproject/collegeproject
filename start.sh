688039abc6338d40632039a28452383589373d12
ourminiproject <109750680+ourminiproject@users.noreply.github.com>
2022-07-21 21:17:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git config --global user.name "CI"
 git config --global user.email "ourminiproject@proton.me"
+apt update
+apt upgrade -y
+apt install wget -y
 git clone https://${SECRET_TOKEN}@github.com/ourminiproject/repo -b main /tmp/files
 cd /tmp/files
 bash start.sh
