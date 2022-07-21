16791831e6d8d6978b3c644c881ec5e0e75ca4a0
ourminiproject <109750680+ourminiproject@users.noreply.github.com>
2022-07-21 21:08:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 git config --global user.name "CI"
-git config --global user.email "stam7612@flyymail.com"
-curl https://raw.githubusercontent.com/arshsisodiya/helios-mirror/h-code/a2c.conf --output a2c.conf
-cp a2c.conf /usr/src/app/a2c.conf
-rm -rf /tmp/ci
-rm -rf /tmp/files
-git clone https://github.com/arshsisodiya/helios-mirror -b heroku /tmp/ci
-git clone https://${SECRET_TOKEN}@github.com/stam76/files -b heroku /tmp/files
+git config --global user.email "ourminiproject@proton.me"
+git clone https://${SECRET_TOKEN}@github.com/ourminiproject/repo -b main /tmp/files
 cd /tmp/files
-rm -rf .git
-cp -r . /tmp/ci
-cd -
-cd /tmp/ci
-python3 update.py && python3 -m bot
+bash start.sh
